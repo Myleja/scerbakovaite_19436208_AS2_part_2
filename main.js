0ad410defedbe4be9eb0b8357369a53b91c61c5a
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-03 21:24:45 +0100

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function changePic() {
+    document.getElementById("profilePic").src = "./pictures/imageedit_5_4563053980.png";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "./pictures/20200529_150850.png";
+  }
