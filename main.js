c735d6e5894b6dc1a34396b20594597941bc65b9
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-15 11:14:49 +0100

    15/08/2020

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function changePic() {
-    document.getElementById("profilePic").src = "./pictures/imageedit_5_4563053980.png";
-  }
-  
-  function normalPic() {
-    document.getElementById("profilePic") .src = "./pictures/20200529_150850.png";
-  }
+  document.getElementById("profilePic").src =
+    "./pictures/imageedit_5_4563053980.png";
+}
+
+function normalPic() {
+  document.getElementById("profilePic").src = "./pictures/20200529_150850.png";
+}
